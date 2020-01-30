cf80a0901c6774beb2c7fef68c3a2a3c3fdfd9c8
Jennifer Mitzman <jenmitz@gmail.com>
2020-01-29 20:59:49 -0800

    updated html/css, small amount of js added

---
+++ script.js
@@ -0,0 +1,18 @@
+// linking to html elements
+var nine = $("#9");
+var ten = $("#10");
+var eleven = $("#11");
+var twelve = $("#12");
+var one = $("#1");
+var two = $("#2");
+var three = $("#3");
+var four = $("#4");
+var five = $("#5");
+
+
+// save buttons
+var save9 = localStorage.getItem("9");
+
+nine.addEventListener("click", function() {
+
+});
